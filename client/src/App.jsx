4f13b69158f39b844b28f0808623f6046d4c37cd
Gabe Harvey <harvey7@gmail.com>
2024-05-01 19:51:26 -0500

    File Structure Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 
 // Pages and Components Required for Application 
-import RootLayout from './layouts/RootLayout'
+import Loading from './layouts/Loading'
 import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Game from './components/Game'
+import MyDesk from './pages/MyDesk'
+import InterviewPrep from './components/InterviewPrep'
 import Login from './components/Login'
 import Signup from './components/Signup'
 
 // Routes
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
+    <Route path='/' element={<Loading />}>
       <Route path='home' element={<Home />} />
-      <Route path='profile' element={<Profile />} />
-      <Route path='game' element={<Game />} />
+      <Route path='mydesk' element={<MyDesk />} />
+      <Route path='interviewprep' element={<InterviewPrep />} />
       <Route path='login' element={<Login />} />
       <Route path='signup' element={<Signup />} />
     </Route>
