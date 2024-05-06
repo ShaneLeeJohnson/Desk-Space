fd3a51200a0cdf0813ec815e6690d0897b6bd4df
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-05-06 18:35:23 -0500

    made style changes to homepage and added new logo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,20 @@
 import { Flex, Box, Heading } from '@chakra-ui/react'
-import DeskSpaceLogo from '../assets/img/deskspace-icon.png';
+import DeskSpaceLogo from '../assets/img/deskspace-logo.png';
 
 export default function Home() {
     return (
-        <Flex flexFlow="column wrap" justifyContent="center" text-align="center">
-            <Box textAlign="center" bg="brand.700" p="15px" m="0">
-                <Heading as="h2" color="brand.800" fontSize="48px" fontWeight="300" mb="5">DeskSpace</Heading>
-                <Heading as="h4" color="brand.900" fontSize="22px" mb="5" fontWeight="500"  lineHeight="1.5">
-                    A space for developers to prep for tech interviews with flashcards and save job listings 
-                </Heading>
-                <Heading as="h3" fontSize="25px" fontWeight="450" color="brand.900">
-                    Please <span><a href="">Login</a></span> or <span><a href="">Signup</a></span> to continue
+        <Flex flexFlow="column wrap" justifyContent="center" text-align="center" bg="brand.800">
+            <Box textAlign="center" p="15px" m="0">
+                {/* <Heading as="h2" color="brand.800" fontSize="48px" fontWeight="300" mb="5">DeskSpace</Heading> */}
+                <Heading as="h4" color="brand.600" fontSize="32px" fontWeight="350" mb="1%" lineHeight="1.5">
+                    Prep for tech interviews and save job listings with...
                 </Heading>
             </Box>
-            <Box alignSelf="center" minW="385px" minH="285px" maxW="55%" maxH="55%" mb="10%" flexGrow="0 1 auto">
+            <Box alignSelf="center"  maxW="75%" maxH="75%" flexGrow="0 1 auto" mb="1%">
                 <img src={DeskSpaceLogo}/>
+                <Heading as="h3" fontSize="28px" fontWeight="350" color="brand.600" textAlign="center" mt="1%" mb="8%">
+                    (Sign-up or Login to continue)
+                </Heading>
             </Box>
         </Flex>
     )
