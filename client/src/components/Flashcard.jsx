b268d776f1bd7e3a3fcceaee84bf8ddf22ef2a85
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-05-07 20:49:04 -0500

    styled flashcards

--- client/src/components/Flashcard.jsx
+++ client/src/components/Flashcard.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Flex, Card } from '@chakra-ui/react'
+import { Flex, Card, Heading, List, Text, ListItem } from '@chakra-ui/react'
 import '../App.css'
 
 export default function Flashcard({ flashcard }) {
@@ -7,26 +7,28 @@ export default function Flashcard({ flashcard }) {
     
     const cardStyle = {
         ':hover': {
-            cursor: 'pointer',
-            animation: 'slide 2s linear'
+            cursor: 'pointer'
         }
     }
 
     // Set flashcards to flip on click
     return (
-    <Flex className={`card ${flip ? 'flip' : ''}`} flexFlow="column wrap" justifyContent="center" alignItems="center" bg="brand.800" color="brand.600"
-        onClick={() => setFlip(!flip)}>
-        <Card sx={cardStyle} flexGrow="0 1 auto"  borderStyle="solid" borderWidth="4px" borderColor="black" m="2%" boxShadow="lg" minW="70%" h="300px" pt="100px" pb="100px">
-        {!flip && (
-            <>
-                <h2 style={{fontFamily:'Nunino', fontWeight:'bolder', fontSize:'20px'}}>{flashcard.question}</h2>
-                <ul style={{listStyleType:'none'}}>{flashcard.choices.map((choice, index) => (<li key={index}>{choice}</li>))}</ul>
-            </>
-        )}
-        {flip && (
-            <h2>{flashcard.answer}</h2>
-        )}
-        </Card>
-    </Flex>
-    )
+        <Flex className={`card ${flip ? 'flip' : ''}`} flexFlow="column wrap" alignItems="center" bg="brand.800" color="brand.600"
+            onClick={() => setFlip(!flip)}>
+            <Card sx={cardStyle} alignItems="center" borderStyle="solid" borderWidth="4px" borderColor="black" my="1%" mb="0" boxShadow="lg" maxW="90%" maxH="90%" minW="60%" minH="300px">
+            {!flip && (
+                <>
+                <Heading flexGrow="0 1 auto" as="h2" fontWeight="450" fontSize="22px" mt="4%" mb="1%" maxW="350px" lineHeight="1.5">{flashcard.question}</Heading>
+                    <List listStyleType='none'>{flashcard.choices.map((choice, index) => (<ListItem key={index} textAlign="left" mt="1">{choice}</ListItem>))}</List>
+                </>
+            )}
+            {flip && (
+                <>
+                <Heading fontSize="24px" fontWeight="500" mt="10" mb="0">Answer</Heading>
+                <Text fontSize="22px" fontWeight="350" textAlign="center" mt="4">{flashcard.answer}</Text>
+                </>
+            )}
+            </Card>
+        </Flex>
+        )
 }
