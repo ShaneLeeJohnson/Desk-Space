4c5cd9e97cb4e841d64ac2ce9f04ecc246a251bb
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-05-08 18:41:44 -0500

    changed wording of heading

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <Box textAlign="center" p="15px" m="0">
                 {/* <Heading as="h2" color="brand.800" fontSize="48px" fontWeight="300" mb="5">DeskSpace</Heading> */}
                 <Heading as="h4" color="brand.600" fontSize="26px" fontWeight="300" mb="1%" lineHeight="1.5" flexShrink="2">
-                    Prep for job interviews with flashcards and save your job listing links with...
+                    Prep for job interviews with flashcards and save your job listing links on...
                 </Heading>
             </Box>
             <Box alignSelf="center"  maxW="90%" maxH="90%" mb="1%" flexShrink="0">
