4a08c142d6decb76d857019bd20d27c66a1dd8b3
shanejohnson <shane.john316@gmail.com>
2024-05-08 19:34:27 -0500

    updated package-lock.json files and created a query to GET_FLASHCARDS in utils/queries.js

--- client/src/components/InterviewPrep.jsx
+++ client/src/components/InterviewPrep.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-// import { useQuery } from '@apollo/client';
+import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client';
 import { Flex, Box, Heading } from '@chakra-ui/react'
 import FlashcardList from './FlashcardList'
+import { GET_FLASHCARDS } from '../utils/queries';
 import '../App.css'
 
 export default function InterviewPrep() {
-    const [flashcards] = useState(SAMPLE_FLASHCARDS)
-    // const { loading, error, data } = useQuery(FLASHCARDS);
-    // const [flashcards, setFlashcards] = useState([]);
+    const { loading, error, data } = useQuery(GET_FLASHCARDS);
+    const [flashcards, setFlashcards] = useState([]);
 
-    // useEffect(() => {
-    //     if (data) {
-    //         setFlashcards(data.flashcards);
-    //     }
-    // }, [data]);
+    useEffect(() => {
+        if (data) {
+            console.log("data", data)
+            setFlashcards(data.flashcards);
+        }
+    }, [data]);
     return ( 
         <>
         <Flex flexFlow="column wrap" justifyContent="center" textAlign="center" bg="brand.800" color="brand.600">
