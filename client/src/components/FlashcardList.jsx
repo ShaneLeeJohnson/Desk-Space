2f1620d96b8ded731e0b85ba67b361ab15abdb66
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-05-09 15:47:28 -0500

    created add/remove card functionality on frontend

--- client/src/components/FlashcardList.jsx
+++ client/src/components/FlashcardList.jsx
@@ -1,9 +1,82 @@
-import { Flex, Box, Button } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Flex, Box, Button, FormControl, Input, Heading } from '@chakra-ui/react';
+import { useState, useRef } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_USER, GET_FLASHCARDS } from '../utils/queries';
+import { ADD_CARD, REMOVE_CARD } from '../utils/mutations';
 import Flashcard from './Flashcard';
 
 export default function FlashcardList({ flashcards }) {
-    console.log(flashcards)
+    console.log(flashcards);
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    const nextCard = () => {
+        setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
+    }
+
+    // Setting variables to store our queries and mutations
+    const [getUser] = [useQuery(GET_USER)];
+    const [getFlashcards] = [useQuery(GET_FLASHCARDS)];
+    const [addCard] = useMutation(ADD_CARD);
+    const [removeCard] = useMutation(REMOVE_CARD);
+    // const [updateCard] = useMutation(UPDATE_CARD);
+
+    // Get user and flashcard data
+    const {data: userData } = getUser;
+    const {data: flashcardsData } = getFlashcards;
+    console.log(userData);
+    console.log(flashcardsData);
+
+    // Creating variables for user form inputs
+    const [formData, setFormData] = useState({ question: "", answer: ""});
+    const inputQuestion = useRef();
+    const inputAnswer = useRef();
+
+    // Function to create a new card
+    const handleAddCard = async () => {
+        try {
+            await addCard({
+                variables: {
+                    question: formData.question,
+                    answer: formData.answer,
+                },
+            });
+            // Clear input fields after adding the card
+            setFormData({ question: "", answer: "" }); 
+            alert('Card added successfully')
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleDeleteCard = async (flashcard) => {
+        const cardId = flashcard._id; // Extract _id from flashcard object
+        try {
+            await removeCard({
+                variables: {
+                    _id: cardId,
+                },
+                refetchQueries: [{ query: GET_FLASHCARDS }],
+            });
+            alert('Card deleted successfully');
+            [currentCardIndex + 1]
+        } catch (error) {
+            console.error('Error deleting card:', error.message);
+            alert('Failed to delete card. Please try again.');
+        }
+}
+
+      // update state based on form input changes
+const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+        ...formData,
+        [name]: value,
+    });
+};
+
+    // async function handleUpdateCard(cardId) {
+
+    // }
+
     const buttonStyle = {
         ':hover': {
             bg: 'brand.600',
@@ -11,21 +84,46 @@ export default function FlashcardList({ flashcards }) {
             border: 'solid 4px black'
         }
     }
-
-    const [currentCardIndex, setCurrentCardIndex] = useState(0);
-
-    const nextCard = () => {
-        setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
-    }
     return (
-        // Shows a flashcard to the user with a "next" button 
-        <Flex bg="brand.900" flexFlow="column wrap" justifyContent="center" textAlign="center" h="70vh">
+        // Shows a flashcard to the user with a "next" button and buttons to edit/delete flashcards
+        <>
+        <Flex bg="brand.900" flexFlow="column wrap" justifyContent="center" textAlign="center" h="75vh">
             {flashcards && flashcards.length > 0 && (
                 <Box color="brand.600">
+                    <Button sx={buttonStyle} onClick={nextCard} p="4" mb="2" color="brand.900" bg="brand.500" border="solid 4px black">Next</Button>
                     <Flashcard flashcard={flashcards[currentCardIndex]} key={flashcards[currentCardIndex]._id} />
-                    <Button sx={buttonStyle} onClick={nextCard} mb="10" mt="2" p="4" color="brand.900" bg="brand.700" border="solid 4px black">Next</Button>
+                    <Button p="2" m="2" color="brand.900" bg="brand.700" border="solid 4px black">Edit Card</Button>
+                    <Button onClick={()=>{handleDeleteCard(flashcards[currentCardIndex])}} p="2" m="2" bg="red.400" border="solid 4px black">Delete Card</Button>
                 </Box>
             )}
         </Flex>
-    )
-}
+        <Flex flexFlow="column wrap" alignItems="center" textAlign="center">
+        <Heading fontSize="26px" fontWeight="400" color="brand.600">Create New Flashcard</Heading>
+            <Box mb="20" p="2" bg="brand.500" borderRadius="10px">
+        <form onSubmit={handleAddCard}>
+            <FormControl>
+                <Input ref={inputQuestion} 
+                placeholder="Enter question" 
+                type="text" 
+                name="question" 
+                value={formData.question} 
+                onChange={handleInputChange}
+                bg="white"
+                color="brand.900"/>
+
+                <Input ref={inputAnswer} 
+                placeholder="Enter answer"
+                type="text" 
+                name="answer"
+                value={formData.answer}
+                onChange={handleInputChange}
+                bg="white"
+                color="brand.900"/>
+            <Button type="submit" m="2" bg="brand.700">Add Card</Button>
+        </FormControl>
+        </form>
+            </Box>
+        </Flex>
+        </>
+        )
+    }
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,3 +26,38 @@ mutation login($username: String!, $password: String!) {
   }
 }
 `;
+
+export const ADD_CARD = gql`
+  mutation addCard($question: String!, $answer: String!) {
+    addCard(question: $question, answer: $answer) {
+    _id
+    email
+    username
+      flashcards {
+      _id
+      question
+      answer 
+    }
+  }
+}
+`;
+
+export const UPDATE_CARD = gql`
+  mutation updateCard($_id: ID!, $question: String!, $answer:String!) {
+    updateCard(_id: $_id, question: $question, answer: $answer) {
+      _id 
+      question
+      answer
+    }
+  }
+`;
+
+export const REMOVE_CARD = gql`
+  mutation removeCard($_id: ID!) {
+    removeCard(_id: $_id) {
+    _id
+    question
+    answer
+    }
+} 
+`;
