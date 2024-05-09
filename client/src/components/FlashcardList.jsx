82175a7ea072e08368ceb2f20620f0ef62232941
shanejohnson <shane.john316@gmail.com>
2024-05-08 20:46:33 -0500

    Fixed code in Flashcard.jsx and FlashcardList.jsx to allow the flashcard app to use the data in mongoDB

--- client/src/components/FlashcardList.jsx
+++ client/src/components/FlashcardList.jsx
@@ -20,12 +20,12 @@ export default function FlashcardList({ flashcards }) {
     return (
         // Shows a flashcard to the user with a "next" button 
         <Flex bg="brand.800" flexFlow="column wrap" justifyContent="center" textAlign="center" mb="4%">
-            {/* {flashcards && flashcards.length > 0 && ( */}
+            {flashcards && flashcards.length > 0 && (
                 <Box color="brand.600">
                     <Flashcard flashcard={flashcards[currentCardIndex]} key={flashcards[currentCardIndex]._id} />
                     <Button sx={buttonStyle} onClick={nextCard} mt="2%" color="brand.600" bg="brand.900">Next Flashcard</Button>
                 </Box>
-            {/* )} */}
+            )}
         </Flex>
     )
 }
