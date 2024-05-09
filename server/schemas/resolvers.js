dd586ddc1a1c8345c97136e8b061af7cc111ff3a
Gabe Harvey <harvey7@gmail.com>
2024-05-08 19:11:38 -0500

    Mutation bug fix in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -33,7 +33,7 @@ const resolvers = {
     },
     addCard: async (parent, args, context) => {
       if (context.user) {
-        const flashCard = await Flashcard.create(args);
+        const flashCard = await Flashcard.create({...args, createdBy: context.user._id});
 
         const user = await User.findByIdAndUpdate(
           context.user._id,
