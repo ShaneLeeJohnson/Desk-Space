b41a6eb16ed31db18729003f47a18ebe7f5b85e0
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-05-13 16:33:20 -0500

    users can't access /mydesk without being logged in

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,79 +1,72 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-import Home from './pages/Home'
-import MyDesk from './pages/MyDesk'
-import FlashcardPage from './components/FlashcardPage.jsx'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Flashcard from "./components/Flashcard"
-import StickyNotes from './components/StickyNotes.jsx'
-
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import MyDesk from "./pages/MyDesk";
+import FlashcardPage from "./components/FlashcardPage.jsx";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Flashcard from "./components/Flashcard";
+import StickyNotes from "./components/StickyNotes.jsx";
+import auth from "./utils/auth.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // Import Chakra
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import './index.css'
-import EditFlashcard from './components/EditFlashcard.jsx'
-
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "./index.css";
+import EditFlashcard from "./components/EditFlashcard.jsx";
 // Router to handle paths to our components/pages
 const router = createBrowserRouter([
   {
-    path: '/', 
+    path: "/",
     element: <App />,
     children: [
       {
         index: true,
-        element: <Home />
+        element: <Home />,
       },
       {
-        path: 'mydesk', 
-        element: <MyDesk />
-      }, 
-      {
-        path: 'flashcards', 
-        element: <FlashcardPage />
+        path: "mydesk",
+        element: auth.loggedIn() ? <MyDesk /> : <Login />,
       },
       {
-        path: 'flashcard', 
-        element: <Flashcard />
+        path: "flashcards",
+        element: auth.loggedIn() ? <FlashcardPage /> : <Login />,
       },
       {
-        path: 'edit/:id', 
-        element: <EditFlashcard />
+        path: "flashcard",
+        element: auth.loggedIn() ? <Flashcard /> : <Login />,
       },
       {
-        path: 'login', 
-        element: <Login />
+        path: "edit/:id",
+        element: auth.loggedIn() ?  <EditFlashcard /> : <Login />,
       },
       {
-        path: 'signup', 
-        element: <Signup />
+        path: "login",
+        element: <Login />,
       },
       {
-        path: 'notes', 
-        element: <StickyNotes />
-      }
-    ]
-  }
-])
-
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "notes",
+        element: auth.loggedIn() ? <StickyNotes /> : <Login />,
+      },
+    ],
+  },
+]);
 // Setting Chakra theme colors
 const colors = {
   brand: {
-    900: '#2D3748', // darkest gray
-    800: '#4A5568', // dark gray
-    700: '#FEEBC8', // light orange
-    600: '#FFFFFF', // white
-    500: '#BEE3F8', // light blue
-  }
-}
-
-
-
-const theme = extendTheme({ colors })
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ChakraProvider theme={theme}>
-      <RouterProvider router={router}/>
-    </ChakraProvider>
-)
+    900: "#2D3748", // darkest gray
+    800: "#4A5568", // dark gray
+    700: "#FEEBC8", // light orange
+    600: "#FFFFFF", // white
+    500: "#BEE3F8", // light blue
+  },
+};
+const theme = extendTheme({ colors });
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ChakraProvider>
+);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,10 +6,10 @@ export default function Home() {
     const isLoggedIn = Auth.loggedIn ();
     return (
         <Flex flexFlow="column wrap" alignItems="center" text-align="center" bg="brand.800" h="100vh">
-                <Heading textAlign="center" as="h4" color="brand.600" fontSize="26px" fontWeight="300" mb="6" lineHeight="1.5" flexShrink="0">
+                <Heading textAlign="center" as="h4" color="brand.600" fontSize="26px" fontWeight="300" mt="2" lineHeight="1.5" flexShrink="0">
                     Create your own flashcard deck and save sticky notes on...
                 </Heading>
-            <Box alignSelf="center" flexShrink="2" w="500px" maxW="80%">
+            <Box alignSelf="center" flexShrink="2" w="500px" maxW="80%" mt="4">
                 <img className="deskspacelogo" src={DeskSpaceLogo} alt='Desktop Logo'/>
                 {!isLoggedIn &&
                     <Text fontSize="24px" fontWeight="350" color="white" textAlign="center" mt="6" mb="5"><a href="/signup" className="links">Sign Up</a> or <a href="login" className="links">Login</a> to continue</Text>
